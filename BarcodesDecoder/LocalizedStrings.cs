76de3a5f407ff292ec6ee592ccb706f5c03e4c56
UNI-TRACKING\Michael <michael.star.mail@gmail.com>
2014-08-07 19:32:52 +0400

    Initial project srtucture

---
+++ BarcodesDecoder/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using BarcodesDecoder.Resources;
+
+namespace BarcodesDecoder
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
