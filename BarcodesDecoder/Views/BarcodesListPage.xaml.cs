9abbb7563cd2bbeb2dd5ce71b8f3db4691225281
UNI-TRACKING\Michael <michael.star.mail@gmail.com>
2014-08-08 18:54:34 +0400

    Russion localization

--- BarcodesDecoder/Views/BarcodesListPage.xaml.cs
+++ BarcodesDecoder/Views/BarcodesListPage.xaml.cs
@@ -29,5 +29,18 @@ namespace BarcodesDecoder.Views
             BindingExpression bindingExpr = textBox.GetBindingExpression(TextBox.TextProperty);
             bindingExpr.UpdateSource();
         }
+
+        private void listCountries_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                var item = (KeyValuePair<string, string>)e.AddedItems[0];
+                MessageBox.Show(String.Format("{0}: {1}", item.Key, item.Value));
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
     }
 }
