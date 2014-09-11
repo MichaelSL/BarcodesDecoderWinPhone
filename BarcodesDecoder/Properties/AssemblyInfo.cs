8391cc2092c17b61f6883e960564b29ab0202bbc
UNI-TRACKING\Michael <michael.star.mail@gmail.com>
2014-09-11 21:05:56 +0400

    Updated version info

--- BarcodesDecoder/Properties/AssemblyInfo.cs
+++ BarcodesDecoder/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Resources;
 [assembly: AssemblyTitle("BarcodesDecoder")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Michael Samorokov")]
 [assembly: AssemblyProduct("BarcodesDecoder")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
